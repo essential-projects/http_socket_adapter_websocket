46653353fc5c06d793f466573925d9c4fd6356fe
Sebastian Meier <sebastian.meier@5minds.de>
2019-03-12 13:59:33 +0100

    add discovery tag

--- ioc_module.js
+++ ioc_module.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const WebsocketHttpSocketAdapter = require('./dist/commonjs/index').WebsocketHttpSocketAdapter;
+const socketAdapterDiscoveryTag = require('@essential-projects/http_contracts').socketAdapterDiscoveryTag;
 
 function registerInContainer(container) {
 
   container.register('WebsocketHttpSocketAdapter', WebsocketHttpSocketAdapter)
     .dependencies('container', 'IdentityService')
     .configure('http:http_extension')
-    .singleton();
+    .singleton()
+    .tags(socketAdapterDiscoveryTag);
 }
 
 module.exports.registerInContainer = registerInContainer;
